b67142392f2174d697338216f9f8a9b2e29cea7a
IgorSerodio <79673301+IgorSerodio@users.noreply.github.com>
2021-08-28 20:19:31 -0300

    Update Espalhamento.hs

--- Haskell/Espalhamento.hs
+++ Haskell/Espalhamento.hs
@@ -3,9 +3,8 @@ module Espalhamento where
 import Queue
 import List
 
-
 espalhamento :: Int -> Int -> Int -> Int -> [Int] -> [Bool] -> [Bool]
-espalhamento x y m n num vis = get_vis (espalhamento_rec m n num ([x + m*y], visitar (x + m*y) vis))
+espalhamento x y m n num vis = get_vis (espalhamento_rec m n num (if ((access (x + n*y) num) > 0) then [x + n*y] else [], visitar (x + n*y) vis))
 
 espalhamento_rec :: Int -> Int -> [Int] -> ([Int], [Bool]) -> ([Int],[Bool])
 espalhamento_rec m n num ([], vis) = ([], vis)
@@ -14,23 +13,23 @@ espalhamento_rec m n num (fila, vis) = espalhamento_rec m n num (dequeue (cima m
 cima :: Int -> Int -> [Int] -> ([Int], [Bool]) -> ([Int], [Bool])
 cima m n num ([], vis) = ([], vis)
 cima m n num (fila, vis)
-  |(top fila) - m < 0 = (fila, vis)
-  |access (top fila - m) vis = (fila, vis)
-  |access ((top fila) - m) num > 0 = (fila, visitar ((top fila) - m) vis)
-  |otherwise = (enqueue ((top fila) - m) fila, visitar ((top fila) - m) vis)
+  |(top fila) - n < 0 = (fila, vis)
+  |access (top fila - n) vis = (fila, vis)
+  |access ((top fila) - n) num > 0 = (fila, visitar ((top fila) - n) vis)
+  |otherwise = (enqueue ((top fila) - n) fila, visitar ((top fila) - n) vis)
 
 baixo :: Int -> Int -> [Int] -> ([Int], [Bool]) -> ([Int], [Bool])
 baixo m n num ([], vis) = ([], vis)
 baixo m n num (fila, vis)
-  |((top fila) + m >= n*m) = (fila, vis)
-  |access (top fila + m) vis = (fila, vis)
-  |access ((top fila) + m) num > 0 = (fila, visitar ((top fila) + m) vis)
-  |otherwise = (enqueue ((top fila) + m) fila, visitar ((top fila) + m) vis)
+  |((top fila) + n >= n*m) = (fila, vis)
+  |access (top fila + n) vis = (fila, vis)
+  |access ((top fila) + n) num > 0 = (fila, visitar ((top fila) + n) vis)
+  |otherwise = (enqueue ((top fila) + n) fila, visitar ((top fila) + n) vis)
 
 direita :: Int -> Int -> [Int] -> ([Int], [Bool]) -> ([Int], [Bool])
 direita m n num ([], vis) = ([], vis)
 direita m n num (fila, vis)
-  |((top fila) + 1 >= n*m) || (div ((top fila) + 1) m) /= (div (top fila) m) = (fila, vis)
+  |((top fila) + 1 >= n*m) || (div ((top fila) + 1) n) /= (div (top fila) n) = (fila, vis)
   |access (top fila + 1) vis = (fila, vis)
   |access ((top fila) + 1) num > 0 = (fila, visitar ((top fila) + 1) vis)
   |otherwise = (enqueue ((top fila) + 1) fila, visitar ((top fila) + 1) vis)
@@ -38,7 +37,7 @@ direita m n num (fila, vis)
 esquerda :: Int -> Int -> [Int] -> ([Int], [Bool]) -> ([Int], [Bool])
 esquerda m n num ([], vis) = ([], vis)
 esquerda m n num (fila, vis)
-  |((top fila) - 1 < 0) || (div ((top fila) - 1) m) /= (div (top fila) m) = (fila, vis)
+  |((top fila) - 1 < 0) || (div ((top fila) - 1) n) /= (div (top fila) n) = (fila, vis)
   |access (top fila - 1) vis = (fila, vis)
   |access ((top fila) - 1) num > 0 = (fila, visitar ((top fila) - 1) vis)
   |otherwise = (enqueue ((top fila) - 1) fila, visitar ((top fila) - 1) vis)
